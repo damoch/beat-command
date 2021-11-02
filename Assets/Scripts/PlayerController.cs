63b1c604962004b4144b2ea4d9c57162082a4d03
Damian Ochramowicz <damochramowicz260@gmail.com>
2021-11-02 21:00:56 +0100

    Initial input and sfx

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public AudioSource Kick;
+        public AudioSource Snare;
+
+        public KeyCode KickKey;
+        public KeyCode SnareKey;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KickKey))
+            {
+                Kick.PlayOneShot(Kick.clip);
+            }
+
+            if (Input.GetKeyDown(SnareKey))
+            {
+                Snare.PlayOneShot(Snare.clip);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RhythmController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RhythmController : MonoBehaviour
+    {
+        public int BPM;
+        public AudioClip HatClip;
+        public AudioSource HiHat;
+        private float _breakTime;
+        private float _elapsed;
+
+        private void Start()
+        {
+            _elapsed = 0;
+            _breakTime = (float)60 / BPM;
+        }
+
+        private void Update()
+        {
+            _elapsed += Time.deltaTime;
+            if(_elapsed < _breakTime)
+            {
+                return;
+            }
+            HiHat.PlayOneShot(HatClip);
+            _elapsed = 0;
+        }
+    }
+}
